7071380a35ef808534710f8810bf421bf3e9f2af
dominicmol <dominicmmol@gmail.com>
2025-06-16 16:30:38 +0200

    Fix baseURL & add Netlify domain to CORS

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const isLocal = host === 'localhost' || host === '127.0.0.1'
 // baseURL instellen
 axios.defaults.baseURL = isLocal
   ? 'http://127.0.0.1:8000/api/v1'                                // lokaal
-  : 'https://dominicmol.pythonanywhere.com/api/v1'                // productie
+  : 'https://dominicmol.pythonanywhere.com'                // productie
 
 const app = createApp(App)
 app.use(store)
